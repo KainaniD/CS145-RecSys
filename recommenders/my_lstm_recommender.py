918f47474e82914af05038e2bd818376661db94f
Justin Tang <125959004+05tangjustin@users.noreply.github.com>
2025-06-16 03:10:49 +0000

    Add sigmoid and parameters to LSTM recommender

--- recommenders/my_lstm_recommender.py
+++ recommenders/my_lstm_recommender.py
@@ -12,6 +12,7 @@ from sim4rec.recommenders.ucb import UCB
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 import sklearn
 from sklearn.tree import DecisionTreeRegressor
@@ -19,11 +20,11 @@ from sklearn.preprocessing import StandardScaler
 from sim4rec.utils import pandas_to_spark
 
 class LSTMModel(nn.Module):
-    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim):
+    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim, dropout):
         super(LSTMModel, self).__init__()
         self.hidden_dim = hidden_dim
         self.layer_dim = layer_dim
-        self.lstm = nn.LSTM(input_dim, hidden_dim, layer_dim, batch_first=True)
+        self.lstm = nn.LSTM(input_dim, hidden_dim, layer_dim, batch_first=True, dropout=dropout)
         self.fc = nn.Linear(hidden_dim, output_dim)
 
     def forward(self, x, h0=None, c0=None):
@@ -32,15 +33,15 @@ class LSTMModel(nn.Module):
             c0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).to(x.device)
         
         out, (hn, cn) = self.lstm(x, (h0, c0))
-        out = self.fc(out[:, -1, :])
+        out = F.sigmoid(self.fc(out[:, -1, :]))
         return out, hn, cn
 
 class LSTMRecommender():
-    def __init__(self, seed=None, epoch_num=10):
+    def __init__(self, seed=None, epoch_num=10, hidden_dim=50, layer_dim=1, dropout=0.0):
         self.seed = seed
         np.random.seed(seed)
 
-        self.model = LSTMModel(input_dim=1, hidden_dim=50, layer_dim=1, output_dim=1)
+        self.model = LSTMModel(input_dim=1, hidden_dim=hidden_dim, layer_dim=layer_dim, output_dim=1, dropout=dropout)
         self.criterion = nn.MSELoss()
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=0.01)
         self.scalar = StandardScaler()
